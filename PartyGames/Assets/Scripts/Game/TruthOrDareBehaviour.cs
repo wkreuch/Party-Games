886abbcfab79e30c069c63e7156c58b9f584db62
Gregory Guilherme Domingos Torres <gregorygdt464@gmail.com>
2018-10-28 23:57:46 -0300

    Mudança no Script TruthOrDare
    Script Reformulado do jogo Verdade ou Desafio

--- PartyGames/Assets/Scripts/Game/TruthOrDareBehaviour.cs
+++ PartyGames/Assets/Scripts/Game/TruthOrDareBehaviour.cs
@@ -7,49 +7,45 @@ public class TruthOrDareBehaviour : MonoBehaviour {
 
     #region Variaves GameObject
 
-    #region GameObject Oculto
-
-    //Para Facilitar a Manipulaçao da Cenas
     private GameObject BG_Dare;
     private GameObject BG_Truth;
     private GameObject Text_Box;
     private GameObject CanvasFinder;
 
-    #endregion
-
     //SceneController Guarda Dados Entre As Cenas
     private GameObject SceneController;
-
-    //Faz a Troca De dados em o BTN
-    public GameObject BTN_Switch;
 
     #endregion
 
     #region Variaves String
 
-    private string callMethods;
-
-
     //Usado para Definir o tipo de txt.file a ser aberto
-    public string file;
-    public string dificuldade;
-    public string fileFinalName;
+    private string file;
+    private string gameDifficulty;
+    private string fetchDifficulty;
+    private string fileFinalName;
 
-    public string[] lines;
+    //Guarda as frases depois de buscar o arquivo correto
+    private string[] lines;
 
     #endregion
 
-    public int random;
+    #region Variaves Int
 
-    void Start ()
+    //Valores para criar evento aleatorio 
+    private int randomDifficulty;
+    private int random;
+
+    #endregion
+
+    public void Start()
     {
-
         #region Parte Responsavel para achar os Game Object
 
         // SceneController Usado para manipular dados entre cenas
         SceneController = GameObject.Find("SceneController");
 
-        //Acha os GameOject da Scena Automatica Mente:
+        //Acha os GameOject da Cenas:
         BG_Dare = GameObject.Find("BG_Dare");
         BG_Truth = GameObject.Find("BG_Truth");
         Text_Box = GameObject.Find("Text_Box");
@@ -57,84 +53,133 @@ public class TruthOrDareBehaviour : MonoBehaviour {
 
         #endregion
 
-        #region Parte Resposanvel por carregar o file.txt correto para cada tipo de ocasião
+        //Busca a Difficuldade Selecionada
+        gameDifficulty = SceneController.GetComponent<DataSave>().Difficulty;
+    }
 
-        dificuldade = SceneController.GetComponent<DataSave>().Difficulty;
-        fileFinalName = file + "_" + dificuldade;
+    //Procura o arquivo correto
+    public void FetchFile()
+    {
+
+        fileFinalName = file + "_" + fetchDifficulty;
         TextAsset optionsText = Resources.Load<TextAsset>(fileFinalName);
+        //Separa o texto por cada quebra de linha
         lines = optionsText.text.Split("\n"[0]);
-
-        #endregion
-    }    
-
-    void Update()
-    {
-
-        #region Ativa a animaçao 
-        //Executa a animaçao
-        //BTNSwitch troca o dados entre os botões
-        if (callMethods == "Truth")
-        {
-            CanvasFinder.GetComponent<Animator>().SetInteger("Animate", 1);
-            Text_Box.GetComponent<UnityEngine.UI.Text>().text = lines[Random.Range(0, lines.Length)];
-            callMethods = "Null";
-        }
-        if (callMethods == "Dare")
-        {
-            CanvasFinder.GetComponent<Animator>().SetInteger("Animate", 1);
-            Text_Box.GetComponent<UnityEngine.UI.Text>().text = lines[Random.Range(0, lines.Length)];
-            callMethods = "Null";
-        }
-        #endregion
-
+        //Chama o Metodo DoAnimation responsavel pela animaçao
+        DoAnimation();
     }
-
-    #region Metodo BTN Verdade
-    //Metodo Chamado ao clicar no botão Verdade
-    public void IsTruthClicked()
+    //Controle do Botão Verdade
+    public void TruthClicked()
     {
-        BG_Truth.SetActive(true);
+        file = "Truth";
+        //Controla o Plano de Fundo Exibido
         BG_Dare.SetActive(false);
-        callMethods = "Truth";
+        BG_Truth.SetActive(true);
+        //Chama o Metodo Randomizer responsavel por ramdomizar o nivel de dificuldade do desafio
+        Randomizer();
     }
-#endregion
-
-    #region Metodo BTN_Desafio
-    //Metodo Chamado ao clicar no botão desafio
-    public void IsDareClicked()
+    //Controle do Botão Desafio
+    public void DareClicked()
     {
-        BG_Dare.SetActive(true);
+        file = "Dare";
+        //Controla o Plano de Fundo Exibido
         BG_Truth.SetActive(false);
-        callMethods = "Dare";
-        
+        BG_Dare.SetActive(true);
+        //Chama o Metodo Randomizer responsavel por ramdomizar o nivel de dificuldade do desafio
+        Randomizer();
     }
-    #endregion
-
-    #region Controle do botão aleatorio
-    public void IsRandom()
+    //Controle do Botão Aleatorio
+    public void RandomClicked()
     {
-        random = Random.Range(0, 2);
-        if(random == 0)
+        //Randomiza um numero
+        //Se for de 0 a 74 chamarar o metodo usado quando o botão verdade e clicado
+        //Se for de 75 a 100 chamarar o metodo usado quando o botão desafio e clicado
+        random = Random.Range(0, 101);
+        if(random >= 0 && random <= 74)
         {
-            BG_Dare.SetActive(false);
-            BG_Truth.SetActive(true);
-            callMethods = "Truth";
+            TruthClicked();
         }
-        if(random == 1)
+        if (random >= 75 && random <= 100)
         {
-            BG_Dare.SetActive(true);
-            BG_Truth.SetActive(false);
-            callMethods = "Dare";
+            DareClicked();
         }
     }
-    #endregion
-
-    #region Metodo BTN_Voltar
-    //Metodo Chamado ao clicar no botão voltar
-    public void IsBackClicked()
+    //Controle do Botão Voltar
+    public void BackClicked()
     {
+        //Adiciona +1 na contagem até passar um anuncio
         CanvasFinder.GetComponent<AdsControler>().CounterADD();
+        //Ativa animação de fechar
         CanvasFinder.GetComponent<Animator>().SetInteger("Animate", 0);
     }
-    #endregion
+    //Controla Animação
+    public void DoAnimation()
+    {
+        //Ativa animação de abrir
+        CanvasFinder.GetComponent<Animator>().SetInteger("Animate", 1);
+        //Chama o metodo responsavel por mudar o texto mostrado no cartão
+        ChangeTextDisplay();
+    }
+    //Muda o Texto a ser exibido
+    public void ChangeTextDisplay()
+    {
+        Text_Box.GetComponent<UnityEngine.UI.Text>().text = lines[Random.Range(0, lines.Length)];
+    }
+    //Randomizer responsavel por ramdomizar o nivel de dificuldade do desafio
+    public void Randomizer()
+    {
+        //Switch usado para controlar o provabilidade do nivel do desafio a ser selecionado
+        switch (gameDifficulty)
+        {
+            case "Easy":
+                fetchDifficulty = "Easy";
+                break;
+            case "Normal":
+                randomDifficulty = Random.Range(0, 101);
+                if (randomDifficulty >= 0 && randomDifficulty <= 50)
+                {
+                    fetchDifficulty = "Easy";
+                }
+                if (randomDifficulty >= 51 && randomDifficulty <= 100)
+                {
+                    fetchDifficulty = "Normal";
+                }
+                break;
+            case "Hard":
+                randomDifficulty = Random.Range(0, 101);
+                if (randomDifficulty >= 0 && randomDifficulty <= 50)
+                {
+                    fetchDifficulty = "Easy";
+                }
+                if (randomDifficulty >= 51 && randomDifficulty <= 75)
+                {
+                    fetchDifficulty = "Normal";
+                }
+                if (randomDifficulty >= 76 && randomDifficulty <= 100)
+                {
+                    fetchDifficulty = "Hard";
+                }
+                break;
+            case "Extreme":
+                randomDifficulty = Random.Range(0, 101);
+                if (randomDifficulty >= 0 && randomDifficulty <= 50)
+                {
+                    fetchDifficulty = "Easy";
+                }
+                if (randomDifficulty >= 51 && randomDifficulty <= 75)
+                {
+                    fetchDifficulty = "Normal";
+                }
+                if (randomDifficulty >= 76 && randomDifficulty <= 90)
+                {
+                    fetchDifficulty = "Hard";
+                }
+                if (randomDifficulty >= 91 && randomDifficulty <= 100)
+                {
+                    fetchDifficulty = "Extreme";
+                }
+                break;
+        }
+        FetchFile();
+    }
 }
